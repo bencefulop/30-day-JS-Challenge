6eed7e33274091b10175b5ea08291d39e4e181e4
Bence Fulop <fbence90@gmail.com>
2020-10-02 18:21:46 +1000

    add clock-like movement to hands

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,11 +1,21 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-  console.log(seconds);
+
+  const minutes = now.getMinutes();
+  const minDegrees = (minutes / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hourDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
