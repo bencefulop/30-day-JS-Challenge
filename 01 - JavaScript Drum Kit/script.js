001f9060b47af2c1d2dba4d055e063754fd2bb99
Bence Fulop <fbence90@gmail.com>
2020-10-02 10:42:06 +1000

    add sounds to play  on keydown

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,9 @@
 window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector('audio');
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  console.log(key);
 });
