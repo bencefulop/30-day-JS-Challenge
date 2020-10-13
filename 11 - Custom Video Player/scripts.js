dfa6de849be62dfc4c66794f839fd84af8632154
Bence Fulop <fbence90@gmail.com>
2020-10-13 15:15:21 +1100

    update progress bar when clicked or dragged

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -30,8 +30,36 @@ function skip() {
   video.currentTime += parseInt(this.dataset.skip);
 }
 
+function handleChangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
+
 toggle.addEventListener('click', togglePlay);
+
 skipButtons.forEach((skipButton) => skipButton.addEventListener('click', skip));
+
+ranges.forEach((range) => range.addEventListener('change', handleChangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener('mousemove', handleChangeUpdate)
+);
