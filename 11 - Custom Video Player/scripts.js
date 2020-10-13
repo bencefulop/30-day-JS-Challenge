d4edb07691509924cc97c777fdc642236b1cbbde
Bence Fulop <fbence90@gmail.com>
2020-10-13 14:07:32 +1100

    play/pause video  when player clicked

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,17 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+video.addEventListener('click', togglePlay);
