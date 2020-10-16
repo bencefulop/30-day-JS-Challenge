757a7dab3439f3affefa772ee547a92b53982e15
Bence Fulop <fbence90@gmail.com>
2020-10-16 17:31:44 +1100

    finish function

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -2,15 +2,15 @@
 // Without this, the checkSlide function would run approx 90+ times if you scroll from top to bottom.
 // With debounce it's only called 6-8 times per top-bottom scroll
 function debounce(func, wait = 20, immediate = true) {
-  var timeout;
+  let timeout;
   return function () {
-    var context = this,
+    let context = this,
       args = arguments;
-    var later = function () {
+    let later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
+    let callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
@@ -19,12 +19,19 @@ function debounce(func, wait = 20, immediate = true) {
 
 const pictures = document.querySelectorAll('.slide-in');
 
-function checkSlide(e) {
-  // determine when we've scrolled halfway throigh the picture
+function checkSlide() {
+  // determine when we've scrolled halfway through the picture
   pictures.forEach((picture) => {
     const slideInAt = window.scrollY + window.innerHeight - picture.height / 2;
     // determine the bottom of image
     const imageBottom = picture.offsetTop + picture.height;
+    const isHalfShown = slideInAt > picture.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      picture.classList.add('active');
+    } else {
+      picture.classList.remove('active');
+    }
   });
 }
 
