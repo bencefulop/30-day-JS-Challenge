e33fc86cdfae2402ff75f0e2d0f087616ad83955
Bence Fulop <fbence90@gmail.com>
2020-10-27 18:38:54 +1100

    play webcam on screen and display it on canvas. Take photo buttons saves pictures to hard drive

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,10 +8,44 @@ function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
-      console.log(localMediaStream);
-      video.scr = window.URL.createObjectURL(localMediaStream);
+      // the below code is deprecated so instead we can attach the media stream directrly to the video source object
+      // video.scr = window.URL.createObjectURL(localMediaStream);
+      video.srcObject = localMediaStream;
       video.play();
+    })
+    .catch((err) => {
+      console.error(`Webcam access is denied`, err);
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    const pixels = ctx.getImageData(0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  // extract data from canvas element
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'goodlooking');
+  link.innerHTML = `<img src="${data}" alt="snapshot" />`;
+  // link.innerText = 'download';
+  // link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+video.addEventListener('canplay', paintToCanvas);
