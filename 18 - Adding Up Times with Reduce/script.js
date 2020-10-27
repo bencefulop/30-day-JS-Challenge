eb4d33cdcdc6c90e12486d1afccf2d0a0502a7e2
Bence Fulop <fbence90@gmail.com>
2020-10-27 17:21:53 +1100

    Method calculates total hours, minutes and seconds of all videos

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -9,3 +9,9 @@ const seconds = timeNodes
     return mins * 60 + secs;
   })
   .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
