02a99b6db828cad354b5a0aa31208cb89ab1404c
Bence Fulop <fbence90@gmail.com>
2020-10-27 17:05:44 +1100

    Use reduce to compress all array elements to one

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -5,6 +5,7 @@ const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 const seconds = timeNodes
   .map((node) => node.dataset.time)
   .map((timeCode) => {
-    const [mins, secs] = timeCode.split(':');
-    console.log(mins, secs);
-  });
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
