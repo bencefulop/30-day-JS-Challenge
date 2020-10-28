266aa348b8c450f7f7ade68bfde6d016d5bb81e2
Bence Fulop <fbence90@gmail.com>
2020-10-28 17:01:15 +1100

    speech recognition function adds new paragraph when you stop talking

--- 20 - Speech Detection/script.js
+++ 20 - Speech Detection/script.js
@@ -10,6 +10,18 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', (e) => {
-  console.log(e.results);
-  const transcript = Array.from(e.results);
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
 });
+
+// listening to end event, so function can run again after you stopped talking.
+recognition.addEventListener('end', recognition.start);
+recognition.start();
